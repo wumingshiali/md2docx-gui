ab598efa240dec851aad6053015e17f6ee2cfd7e
wumingshiali <157328007+wumingshiali@users.noreply.github.com>
2025-05-29 21:07:54 +0800

    feat(gui): 添加图形用户界面支持
    - 实现了基本的图形用户界面，包括文件选择、目录选择和转换按钮
    - 增加了剪贴板内容自动检测和使用功能
    - 优化了错误处理和用户提示信息
    - 更新了 README.md，移除了待办事项中的图形用户界面开发

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import tkinter.messagebox
 import maliang as m
 import tkinter.filedialog
 from tkinter import messagebox
+import pyperclip
+import winreg
+
 # 添加项目根目录到 Python 路径
 # sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
@@ -32,32 +35,43 @@ def selectFloder():
 
 
 def covert():
-    global pathf_, path_
-    if not path_:
+    global pathf_, path_, con
+    if (not path_) and con == None:
         tkinter.messagebox.showwarning("请先选择文件！")
         return
     try:
-        # 修正编码参数为utf-8-sig并保留错误处理
-        with open(path_, "r", encoding="utf-8-sig", errors='ignore') as f:
-            c = f.read()
+        if con == None:
+            with open(path_, "r", encoding="utf-8-sig", errors='ignore') as f:
+                c = f.read()
+        else:
+            c = con
         coverter = BaseConverter()
         doc = coverter.convert(c)
 
         if pathf_ == None:
             pathf_ = os.path.dirname(path_)
-        output_path = os.path.join(pathf_, os.path.splitext(os.path.basename(path_))[0] + ".docx")
+        if con != None:
+            output_path = winreg.QueryValueEx(winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"), "Personal")[0] + "\\" + "output.docx"
+        else:
+            output_path = os.path.join(pathf_, os.path.splitext(os.path.basename(path_))[0] + ".docx")
         doc.save(output_path)
     except PermissionError:
         tkinter.messagebox.showerror(f"权限错误：无法保存到路径 '{pathf_}'，请检查路径是否存在且有写入权限。")
     except Exception as e:
-        tkinter.messagebox.showerror(f"保存文件时发生错误: {e}")
+        tkinter.messagebox.showerror(f"保存文件时发生错误",e)
     finally:
-        tkinter.messagebox.showinfo(f"转换完成：已保存为 '{output_path}'")
+        tkinter.messagebox.showinfo("OK","转换完成：已保存")
 global win,cv
 win = m.Tk(title="Markdown 转 Word")    
 cv = m.Canvas(auto_zoom=True)
 cv.place(width=1280, height=720)
 m.Button(cv,(20,20),text="选择Markdown文件", command=selectPath)
+if pyperclip.paste():
+    con = pyperclip.paste()
+    m.Text(cv,(280,25),text="已获取剪切板内容，自动使用剪贴板内容进行转换，自动保存在文档文件夹，文件名为output.docx")
+else:
+    con = None
+    m.Text(cv,(20,60),text="未获取剪切板内容，请手动选择Markdown文件进行转换")
 m.Button(cv,(20,60),text="选择Docx文件保存目录", command=selectFloder)
 m.Text(cv,(280,65),text="可不选，默认为和原文件同目录")
 m.Button(cv,(20,100),text="开始转换", command=covert)
